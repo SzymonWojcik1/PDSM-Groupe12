a557c67c383d78b6d01f9e20e2aa60fb51c425b8
Yann Husmann <yann.husmann@hes-so.ch>
2025-05-05 14:27:15 +0200

    Ajout de la version release V1

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://91.108.184.95:8000/api';
 
 export const api = axios.create({
   baseURL: BASE_URL,
